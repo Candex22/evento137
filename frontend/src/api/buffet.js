39ef726f1e0d3c614fa6d9a4e7be57bd83900055
Candex22 <129701042+Candex22@users.noreply.github.com>
2026-06-06 14:30:37 -0300

    cueck

--- frontend/src/api/buffet.js
+++ frontend/src/api/buffet.js
@@ -1,4 +1,5 @@
-import { get, post } from './client'
+import { get, post, patch } from './client'
 
-export const getProductos   = ()                 => get('/buffet/productos')
-export const crearIngreso   = (nombre, cantidad) => post('/buffet/ingresos', { nombre, cantidad })
+export const getProductos = ()                 => get('/buffet/productos')
+export const crearIngreso = (nombre, cantidad) => post('/buffet/ingresos', { nombre, cantidad })
+export const ajustarStock = (id, delta)        => patch(`/buffet/productos/${id}/ajustar`, { delta })
