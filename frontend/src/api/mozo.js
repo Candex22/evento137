6cad49969927ea49b019c534a37e6929ddcf4dbf
Candex22 <129701042+Candex22@users.noreply.github.com>
2026-06-06 16:40:40 -0300

    como queria yo que funque

--- frontend/src/api/mozo.js
+++ frontend/src/api/mozo.js
@@ -1,5 +1,6 @@
 import { get, post } from './client'
 
-export const getProductos = ()             => get('/mozo/productos')
-export const crearPedido   = (nombre, items) => post('/mozo/pedidos', { nombre, items })
-export const misPedidos    = ()             => get('/mozo/pedidos')
+export const getProductos = ()        => get('/mozo/productos')
+export const misPedidos   = ()        => get('/mozo/pedidos')
+export const crearPedido  = (nombre, items, metodo_pago, recibido) =>
+  post('/mozo/pedidos', { nombre, items, metodo_pago, recibido })
