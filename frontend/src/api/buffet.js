c4504dab7e0092895710eb2fcc5d2785c61054c5
Candex22 <129701042+Candex22@users.noreply.github.com>
2026-06-06 15:47:04 -0300

    simpre es buffet.js

--- frontend/src/api/buffet.js
+++ frontend/src/api/buffet.js
@@ -1,5 +1,9 @@
 import { get, post, patch } from './client'
 
-export const getProductos = ()                 => get('/buffet/productos')
-export const crearIngreso = (nombre, cantidad) => post('/buffet/ingresos', { nombre, cantidad })
-export const ajustarStock = (id, delta)        => patch(`/buffet/productos/${id}/ajustar`, { delta })
+export const getProductos     = ()                          => get('/buffet/productos')
+export const crearProducto    = (nombre, precio, tiene_gustos) => post('/buffet/productos', { nombre, precio, tiene_gustos })
+export const agregarGusto     = (id, nombre, stock)         => post(`/buffet/productos/${id}/gustos`, { nombre, stock })
+export const cargarStock      = (payload)                   => post('/buffet/ingresos', payload)
+export const ajustarProducto  = (id, delta)                 => patch(`/buffet/productos/${id}/ajustar`, { delta })
+export const ajustarGusto     = (id, delta)                 => patch(`/buffet/gustos/${id}/ajustar`, { delta })
+export const actualizarPrecio = (id, precio)               => patch(`/buffet/productos/${id}/precio`, { precio })
